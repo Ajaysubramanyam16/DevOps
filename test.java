01bf9b6c468a45d111d3700bc00108f3e63944e5
Ajaysubramanyam16 <157373427+Ajaysubramanyam16@users.noreply.github.com>
2025-03-10 10:05:53 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,27 +1,12 @@
-public class test {
+class Main {
 
   public static void main(String[] args) {
+    
+    int first = 10;
+    int second = 20;
 
-    int num = 29;
-    boolean flag = false;
-
-    // 0 and 1 are not prime numbers
-    if (num == 0 || num == 1) {
-        flag = true;
-    }
-
-    for (int i = 2; i <= num / 2; ++i) {
-
-      // condition for nonprime number
-      if (num % i == 0) {
-        flag = true;
-        break;
-      }
-    }
-
-    if (!flag)
-      System.out.println(num + " is a prime number.");
-    else
-      System.out.println(num + " is not a prime number.");
+    // add two numbers
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
   }
 }
